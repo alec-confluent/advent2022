567b90d42ce39b7ee5d1548c96e817cdc60bf0d6
Alec Ellebracht <aellebracht@confluent.io>
2022-12-05 18:09:06 -0800

    moved adventOfCode requests to module

--- adventDay1/main.go
+++ adventDay1/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
+	"utils/adventofcode"
 )
 
 var client http.Client
@@ -43,7 +40,7 @@ type Item struct {
 func main() {
 
 	// check(err) // does some error handling
-	data := getData("https://adventofcode.com/2022/day/1/input")
+	data := adventofcode.GetData("https://adventofcode.com/2022/day/1/input")
 
 	// fmt.Println("response data:")
 	// fmt.Println(data)
@@ -99,55 +96,4 @@ func readData(data string) []Elf {
 	}
 
 	return elves
-}
-
-// gets the excercise data from the code advent site
-func getData(address string) string {
-
-	// open up the json file to get our session key
-	jsonFile, err := os.Open("../info.json")
-
-	if err != nil {
-		log.Fatalf("Error... %s", err.Error())
-	}
-	defer jsonFile.Close()
-
-	jsonBytes, _ := ioutil.ReadAll(jsonFile)
-
-	var result map[string]interface{}
-	json.Unmarshal([]byte(jsonBytes), &result)
-
-	sessionId := fmt.Sprintf("%v", result["key"])
-
-	// create a cookie with the session id for the request
-	sessionCookie := &http.Cookie{
-		Name:   "session",
-		Value:  sessionId,
-		MaxAge: 300,
-	}
-
-	// create an http request for the excercise data
-	req, err := http.NewRequest("GET", address, nil)
-	if err != nil {
-		log.Fatalf("Error... %s", err.Error())
-	}
-
-	req.AddCookie(sessionCookie)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error... %s", err.Error())
-	}
-	defer resp.Body.Close()
-
-	fmt.Printf("response status: %d\n", resp.StatusCode)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error... %s", err.Error())
-	}
-
-	stringBody := string(body[:])
-
-	return stringBody
 }
